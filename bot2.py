e87db1f5aff481479c7e70acd54be07428cd3e58
magdalena1982 <magdalena.k.majchrzak@gmail.com>
2025-05-15 07:53:46 +0200

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,31 +1,30 @@
-from telethon.sync import TelegramClient
+import asyncio
+from telethon import TelegramClient
 from telethon.tl.types import UserStatusOnline
 import requests
 import time
 
-api_id = 20375615  # wpisz swój
-api_hash = 'a42c3d95a903f070a77dc8fdea40a6e4'
-bot_token = '7571531890:AAEZkeQNUrK4MhowVPdOPVCiXV0Nh6w2RKc'
-chat_id = 7029563819 # ID Twojego czatu (możemy zdobyć)
-user_id_to_track = 5000291224  # ID osoby, którą chcesz śledzić
-
-was_online = False
-
-from telethon import TelegramClient
 api_id = 20375615
 api_hash = 'a42c3d95a903f070a77dc8fdea40a6e4'
 bot_token = '7571531890:AAEZkeQNUrK4MhowVPdOPVCiXV0Nh6w2RKc'
-client = TelegramClient('bot', api_od, api_hash).start(bot_token=bot_token)
+chat_id = 7029563819
+user_id_to_track = 5000291224
 
-#with TelegramClient('session_name', api_id, api_hash) as client:
+was_online = False
+
+client = TelegramClient('bot', api_id, api_hash)
+
+async def main():
+    await client.start(bot_token=bot_token)
     print("Bot uruchomiony. Czekam na status online...")
+
+    global was_online
+
     while True:
-        user = client.get_entity(user_id_to_track)
-        user_info = client.get_peer_id(user)
+        user = await client.get_entity(user_id_to_track)
         status = user.status
 
         if isinstance(status, UserStatusOnline) and not was_online:
-            # Wyślij powiadomienie przez bota
             message = f"{user.first_name} jest ONLINE!"
             url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
             requests.post(url, data={'chat_id': chat_id, 'text': message})
@@ -34,4 +33,7 @@ client = TelegramClient('bot', api_od, api_hash).start(bot_token=bot_token)
         if not isinstance(status, UserStatusOnline):
             was_online = False
 
-        time.sleep(10)  # sprawdza co 10 sekund
+        await asyncio.sleep(10)
+
+with client:
+    client.loop.run_until_complete(main())
